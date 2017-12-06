6884449d89d444265de25c7f4f5df67026ef0459
marizaswart <mariza.swart@gmail.com>
2017-12-06 10:05:43 +0100

    Test validation for email and password with unit test, all green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column :encrypted_password}
   end
 
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+  end
+
   describe 'factory' do
     it 'is valid' do
       expect(FactoryBot.create(:user)).to be_valid
