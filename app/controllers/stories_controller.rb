1b679ebd348a9a0aaf138924664d0755cd4b8d23
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-10 22:42:03 +0100

    Refactor to make implementation work for presentation purposes, needs revision

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,10 @@
 class StoriesController < ApplicationController
+  def index
+    @stories = Story.all
+  end
+
   def show
+    @stories = Story.all
     @story = Story.find(params[:id])
     @first_chapter = @story.chapters.first
   end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @stories = Story.all
+    @first_story = Story.find_by(title: "Starship")
   end
 end
