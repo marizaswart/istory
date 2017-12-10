9d4a0d004b41bbd2ec105487869744f311cae79a
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-10 22:19:58 +0100

    Revert seeds to previous state due to new syntax breaking

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,5 @@
 class StoriesController < ApplicationController
-  def index
-    @stories = Story.all
-  end
-
   def show
-    @stories = Story.all
     @story = Story.find(params[:id])
     @first_chapter = @story.chapters.first
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-chapters = [{"parent_chapter": "", "title": "The story begins", "content": "There is a huge asteroid coming your way, what do you do?"},
- {"parent_chapter": "The story begins", "title": "Shoot it with a laser", "content": "Now a monster emerges from inside the asteroid, wyd?"},
- {"parent_chapter": "The story begins", "title": "Hide", "content": "The asteroid hits the starship, wyd?"},
- {"parent_chapter": "Shoot it with a laser", "title": "Talk", "content": "The monster says it wants to kill you, wyd?"},
- {"parent_chapter": "Shoot it with a laser", "title": "Throw a grenade", "content": "The monster explodes, the whole room is now messy, wyd?"},
- {"parent_chapter": "Shoot it with a laser", "title": "Run", "content": "The monster runs after you. Go right or left?"},
- {"parent_chapter": "Run", "title": "Right", "content": "The monster got stuck and you got away. The end"}]
+chapters = [{"parent_chapter"=>"", "title"=>"The story begins", "content"=>"There is a huge asteroid coming your way, what do you do?"},
+  {"parent_chapter"=>"The story begins", "title"=>"Shoot it with a laser", "content"=>"Now a monster emerges from inside the asteroid, wyd?"},
+  {"parent_chapter"=>"The story begins", "title"=>"Hide", "content"=>"The asteroid hits the starship, wyd?"},
+  {"parent_chapter"=>"Shoot it with a laser", "title"=>"Talk", "content"=>"The monster says it wants to kill you, wyd?"},
+  {"parent_chapter"=>"Shoot it with a laser", "title"=>"Throw a grenade", "content"=>"The monster explodes, the whole room is now messy, wyd?"},
+  {"parent_chapter"=>"Shoot it with a laser", "title"=>"Run", "content"=>"The monster runs after you. Go right or left?"},
+  {"parent_chapter"=>"Run", "title"=>"Right", "content"=>"The monster got stuck and you got away. The end"}]
 
-stories = [{"title": "Starship", "author": "Mariza & Alfred", "intro": "Once upon a time, in a far away galaxy. There was a young team on the Discovery Starship"}]
+stories = [{"title"=>"Starship", "author"=>"Mariza & Alfred", "intro"=>"Once upon a time, in a far away galaxy. There was a young team on the Discovery Starship"}]
 
 stories.each do |hash|
   Story.create(hash)
 end
 
 chapters.each do |hash|
- parent = Chapter.find_by(title: hash["parent_chapter"])
- story = Story.find_by(title: "Starship")
- Chapter.create(hash.merge!(story: story, parent_chapter: parent).except!("parent_chapter"))
+  parent = Chapter.find_by(title: hash["parent_chapter"])
+  story = Story.find_by(title: "Starship")
+  Chapter.create(hash.merge!(story: story, parent_chapter: parent).except!("parent_chapter"))
 end
